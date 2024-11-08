4b489d3509ec8b92db4184b3676f606f09bdae0b
mohammad-mursalin <mursalinlamon@gmail.com>
2024-11-08 14:42:31 +0600

    controller class

---
+++ src/main/java/com/mursalin/challenge_tracker/controller/ChallengeController.java
@@ -0,0 +1,9 @@
+package com.mursalin.challenge_tracker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/challenge")
+public class ChallengeController {
+}
