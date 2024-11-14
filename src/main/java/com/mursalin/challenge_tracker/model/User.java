003e271054630f55c9235473a286ccb2ebc000a9
mohammad-mursalin <mursalinlamon@gmail.com>
2024-11-14 13:24:03 +0600

    working on role based authentication

--- src/main/java/com/mursalin/challenge_tracker/model/User.java
+++ src/main/java/com/mursalin/challenge_tracker/model/User.java
@@ -19,4 +19,6 @@ public class User {
     private String user_mail;
     @NonNull
     private String password;
+
+    private String role = "USER";
 }
--- src/main/java/com/mursalin/challenge_tracker/model/UserPrinciples.java
+++ src/main/java/com/mursalin/challenge_tracker/model/UserPrinciples.java
@@ -1,9 +1,11 @@
 package com.mursalin.challenge_tracker.model;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class UserPrinciples implements UserDetails {
@@ -16,36 +18,36 @@ public class UserPrinciples implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return Collections.singleton(new SimpleGrantedAuthority(user.getRole()));
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return user.getUser_mail();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
     }
 }
